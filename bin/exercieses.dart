bf882f677a79eb51304ac43a279d988278e2dcb4
FullFlutter.uz <89080884+BrightCodersofRishtan@users.noreply.github.com>
2022-11-15 02:27:34 -0800

    Exercises 1

--- bin/exercieses.dart
+++ bin/exercieses.dart
@@ -1,5 +1,17 @@
 import 'package:exercieses/exercieses.dart' as exercieses;
+import 'dart:io';
+
+/*ex1
+foydalanuvchi ismini kiritadi 
+yoshini suraymiz va nechi yoshga kirganini print qil!
+*/
 
 void main(List<String> arguments) {
-  print('Hello world: ${exercieses.calculate()}!');
+  stdout.write("What is your name?__");
+  String? name = stdin.readLineSync();
+  print("Hi $name, what is your age?__");
+  int age = int.parse(stdin.readLineSync()!);
+
+  int curentAge = 2022 - age;
+  print("Hi $name, your age $curentAge!");
 }
